31d4f80f1d16fceef095ff83baa492bafe95893d
divan56 <arraylist960@gmail.com>
2021-09-30 19:18:43 +0500

    added DAO for store

--- bin/custom/marketstrail/src/markettrail/daos/StoreDAO.java
+++ bin/custom/marketstrail/src/markettrail/daos/StoreDAO.java
@@ -1,4 +1,11 @@
 package markettrail.daos;
 
+import markettrail.model.CustomStoreModel;
+
+import java.util.List;
+
 public interface StoreDAO {
+    List<CustomStoreModel> getAllStores();
+
+    List<CustomStoreModel> getStoreByCode(String code);
 }
